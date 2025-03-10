c292e38defa1a30e9efb5c2e698bb7f070e02dc2
Laboratório FIAP <labsfiap@fiap.com.br>
2025-03-10 20:53:37 -0300

    aula2

--- study-apiw-old/src/main/java/com/github/vanaweb/study_apiw/ControllerProduto.java
+++ study-apiw-old/src/main/java/com/github/vanaweb/study_apiw/ControllerProduto.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,22 +14,22 @@ import org.springframework.web.bind.annotation.RestController;
 public class ControllerProduto {
 
     @PostMapping
-    public ResponseEntity<String> create(){
+    public ResponseEntity<String> create(@RequestBody Produto produto) {
         return ResponseEntity.status(201).body("Produto cadastrado: maça");
     }
 
     @PutMapping
-    public ResponseEntity<String> update(){
+    public ResponseEntity<String> update() {
         return ResponseEntity.status(200).body("Produto atualizado");
     }
 
     @GetMapping
-    public ResponseEntity<String> find(){
+    public ResponseEntity<String> find() {
         return ResponseEntity.status(200).body("Maça");
     }
 
     @DeleteMapping
-    public ResponseEntity<Void> delete(){
+    public ResponseEntity<Void> delete() {
         return ResponseEntity.status(204).build();
 
     }
